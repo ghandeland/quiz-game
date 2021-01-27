9697d6fb5607a61fa5e236ea91285be3e0d8ec67
ghandeland <gabriel.handeland@gmail.com>
2021-01-27 13:12:46 +0100

    Full test

--- tests/quizzes-test.js
+++ tests/quizzes-test.js
@@ -3,6 +3,7 @@ const {getRandomQuizzes} = require("../src/quizzes");
 test("Test invalid n", () => {
     
     expect(() => getRandomQuizzes(-1)).toThrow();
-    
-    
+    expect(() => getRandomQuizzes(0)).toThrow();
+    expect(() => getRandomQuizzes(99999)).toThrow();
 }) 
+
